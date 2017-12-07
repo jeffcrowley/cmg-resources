3aacd1952b929767f437448e3c15b78ed1d0bb8d
jeffcrowley <jeffcrowley@Jeffs-MacBook-Pro.local>
2017-12-07 12:24:39 -0800

    add basic admin pages

--- app/controllers/admin_pages_controller.rb
+++ app/controllers/admin_pages_controller.rb
@@ -1,13 +1,19 @@
 class AdminPagesController < ApplicationController
   def main
+   @users = User.all
+   @projects = Project.all
+   @follows = Follow.all
   end
 
   def users
+   @users = User.all
   end
 
   def projects
+   @projects = Project.all
   end
 
   def follows
+   @follows = Follow.all
   end
 end
