461e267e57295f823eb6910b415d4d52201079bc
jeffcrowley <jeffcrowley@Jeffs-MacBook-Pro.local>
2017-12-24 08:29:17 -0800

    improve project page layout, add breadcrumbs to project change orders page

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,16 +3,19 @@ class Project < ApplicationRecord
    has_many :users, through: :follows
    has_many :change_orders
 
-   def current_contract
+   def approved_change_order_total
+      co_amount = 0.0
       if self.change_orders.any?
-         co_amount = 0.0
          self.change_orders.each do |change_order|
             co_amount += change_order.approved_co_value
          end
-         return co_amount + self.original_contract_amount
-      else
-         return self.original_contract_amount
       end
+      return co_amount
+   end
+
+
+   def current_contract
+      self.original_contract_amount + self.approved_change_order_total
    end
 
 end
